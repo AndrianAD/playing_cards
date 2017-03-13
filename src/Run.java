1d8ebc7ccfd90fc275ebe8d67282bbbd77f988ee
Andrian <druc_andrian@mail.ru>
2017-03-14 00:28:51 +0200

    1

--- src/Run.java
+++ src/Run.java
@@ -3,8 +3,15 @@
  */
     public class Run {
     public static void main(String[] args) {
+    Suit[]masti=new Suit[4];
+    for(int i=0;i<4;i++)
+        masti[i]=Suit.values[i];
+        System.out.println(masti);
 
 
+
+//        Card card=new Card(Rank.ACE,Suit.CLUBS);
+//        System.out.println(card.getRank().getName());
     }
 
     }

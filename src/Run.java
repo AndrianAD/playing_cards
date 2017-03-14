a50df13d5f540106d6ccbd77e7299198917ea2a2
AndrianAD <druc_andrian@mail.ru>
2017-03-14 17:41:16 +0200

    170314

--- src/Run.java
+++ src/Run.java
@@ -3,8 +3,11 @@
  */
     public class Run {
     public static void main(String[] args) {
-
-
+        Card card=new Card(Rank.ACE,Suit.CLUBS);
+        System.out.println(card.getSuit().getName()+ " " +card.getRank().getName());
+        card.setRank(Rank.TEN);
+        card.setSuit(Suit.DIAMONDS);
+        System.out.println(card.getSuit().getName()+ " " +card.getRank().getName());
     }
 
     }

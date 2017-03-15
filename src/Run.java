32350b48aa57f62c3651e9b10427a81e1697cc82
AndrianAD <druc_andrian@mail.ru>
2017-03-15 13:47:01 +0200

    170315

--- src/Run.java
+++ src/Run.java
@@ -1,13 +1,27 @@
+import java.util.ArrayList;
+
 /**
  * Created by User on 170313.
  */
     public class Run {
+    private static ArrayList mCardList=new ArrayList();
     public static void main(String[] args) {
-        Card card=new Card(Rank.ACE,Suit.CLUBS);
-        System.out.println(card.getSuit().getName()+ " " +card.getRank().getName());
-        card.setRank(Rank.TEN);
-        card.setSuit(Suit.DIAMONDS);
-        System.out.println(card.getSuit().getName()+ " " +card.getRank().getName());
+        mCardList = new ArrayList<>();
+        Rank[] ranks = Rank.values;
+        Suit[] suites = Suit.values;
+
+        for (int i = 0; i < ranks.length; i++) {
+            for (int j = 0; j < suites.length; j++) {
+                Card card = new Card(ranks[i], suites[j]);
+                mCardList.add(card);
+                System.out.println(card.getSuit().getName() + " " + card.getRank().getName());
+                int a=mCardList.size();
+                System.out.println(a);
+            }
+        }
+
+
+
     }
 
     }
